f35b0b180144d467b79a46db7c17fa945f76ec90
wahyunurdian26 <wahyunurdian98@gmail.com>
2024-09-04 22:26:02 +0700

    first commit

---
+++ src/page/HomeView.jsx
@@ -0,0 +1,28 @@
+import CartProduct from "../components/CartProduct";
+import customAPI from "../api";
+import { useLoaderData } from "react-router-dom";
+
+export const loader = async ({ request }) => {
+  const { data } = await customAPI.get("/product");
+
+  const products = data.data;
+  return { products };
+};
+
+const HomeView = () => {
+  const { products } = useLoaderData();
+  return (
+    <>
+      <div className="border-b border-primary pb-5">
+        <h2 className="text-2xl font-bold capitalize">product list</h2>
+      </div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5">
+        {products.map((item) => (
+          <CartProduct item={item} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default HomeView;
