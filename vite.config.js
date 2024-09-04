2f3cb3de4a0dc6cf03ebe252db6c005ed36abb2b
wahyunurdian26 <wahyunurdian98@gmail.com>
2024-09-04 21:34:30 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://be-toko-phi.vercel.app",
-        changeOrigin: true,
+export default ({ mode }) => {
+  // Menggabungkan environment variables
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  // Konfigurasi Vite
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: process.env.VITE_API_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
-});
+  });
+};
