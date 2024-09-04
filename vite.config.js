d4757713df45467c1b7e433863535dbcca56fd63
wahyunurdian26 <wahyunurdian98@gmail.com>
2024-09-04 19:29:56 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://be-toko-phi.vercel.app",
         changeOrigin: true,
       },
     },
