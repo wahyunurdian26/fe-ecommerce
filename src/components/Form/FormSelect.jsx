496945bea566d8550221252e0c999ffcb341cda0
wahyunurdian26 <wahyunurdian98@gmail.com>
2024-09-05 09:12:35 +0700

    FILTER

--- src/components/Form/FormSelect.jsx
+++ src/components/Form/FormSelect.jsx
@@ -11,15 +11,11 @@ const FormSelect = ({ label, name, list, defaultValue }) => {
         className="select select-bordered"
         defaultValue={defaultValue}
       >
-        {list && list.length > 0 ? (
-          list.map((item) => (
-            <option key={item} value={item}>
-              {item}
-            </option>
-          ))
-        ) : (
-          <option disabled>No options available</option>
-        )}
+        {list.map((item) => (
+          <option key={item} value={item}>
+            {item}
+          </option>
+        ))}
       </select>
     </div>
   );
