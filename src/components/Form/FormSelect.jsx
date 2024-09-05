374d9dadb1f0396419691b0a6250a3cd36484190
wahyunurdian26 <wahyunurdian98@gmail.com>
2024-09-05 09:29:35 +0700

    FILTER

--- src/components/Form/FormSelect.jsx
+++ src/components/Form/FormSelect.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const FormSelect = (label, name, list, defaultValue) => {
+const FormSelect = ({ label, name, list, defaultValue }) => {
   return (
     <div className="form-control">
       <label className="label">
